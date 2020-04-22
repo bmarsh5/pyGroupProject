3f55611188de6a23520bef1d7119e243a33fe68d
bmarsh5 <62040820+bmarsh5@users.noreply.github.com>
2020-04-22 02:14:16 -0600

    Add files via upload

--- nimGame.py
+++ nimGame.py
@@ -1,12 +1,12 @@
 """
-Name:
+Name: Bryan Marsh
 CSC 119-001
-Date:040220
+Date:04/20/20
 Program Name:nimGame
 Description:
-            From a pile of marbles, a player or the computer may take up to
-            half of the marbles on each turn.  The player who takes the last
-            marble loses.
+            From a pile of balls, a player or the computer may take up to
+            half of the balls on each turn.  The player who takes the last
+            balls loses.
             You will note that the computer cannot be beaten in smart mode when it has the
             first move, unless the pile size happens to be 15, 31, or 63. 
             Of course, a human player who has the first turn and knows the winning strategy
@@ -30,41 +30,43 @@ def main():
     
     game = playGame()
     
-##the main function that starts the game
+##The main function that starts the game
 # 
-# there is no return.  The game is played within this function.
+# There is no return.  The game is played within this function.
 # Bryan and some of Jim        
 def playGame() :
     ballCount = ballCountFunction()
     whoseTurn = turnFunction()
     mode = computerMode()
+    print()
     
    
     while ballCount >= 2 :
         if whoseTurn == 0 :
             #PC turn
+            print("Computer's turn.  ", end="")
             if mode == 0 or ballCount == 3 or ballCount == 7 or ballCount == 15 or ballCount == 31 or ballCount == 63 :
-                print("computer acting in dumb mode")
+                print("Computer is playing in dumb mode.")
                 ballCount = computerDumbMode(ballCount)
-                print("The ball count is now",ballCount, "\n")
+                print("The pile size is now",ballCount, "balls.\n")
                 whoseTurn = 1
             else :
-                print("computer acting in smart mode")
+                print("Computer is playing in smart mode.")
                 ballCount = computerSmartMode(ballCount)
                 whoseTurn = 1
-                print("Computer removed balls to make the pile:", ballCount,"balls\n")
+                print("Computer removed balls to make the pile", ballCount,"balls.\n")
         else :
             #Player turn
-            print("Player's turn")
+            print("Player's turn.")
             ballCount = playerTurn(ballCount)
-            print("The ball count is now:",ballCount,"balls\n")
+            print("The ball count is now:",ballCount,"balls.\n")
             whoseTurn = 0
             
     if ballCount == 1 :
         if whoseTurn == 0 :
             print("Computer loses and Player wins!!!!")
         else :
-            print("Player loses and Computer wins")
+            print("Player loses and Computer wins.")
                   
 ##Generate a random integer of 0 or 1 for first turn.
 # @turn is the random 0 or 1.
@@ -81,7 +83,7 @@ def turnFunction() :
 # Jim
 def ballCountFunction() :
         result = randint(10,100)
-        print("Ball count is ",result)
+        print("Ball count is ",result,"balls.")
         return result
     
 ## Computes a 0 or 1 to determine dumb or smart mode.
@@ -90,7 +92,7 @@ def ballCountFunction() :
 # Jim
 def computerMode() :
     computerMode = randint(0,1)
-    print("Computer =", computerMode)
+    print("Computer mode =", computerMode)
     return computerMode
 
 ## Computes the ballcount while in dumb mode
@@ -102,14 +104,13 @@ def computerDumbMode(ballCount):
     halfOfBalls = ballCount//2     
     dumbBallCount = randint(1,halfOfBalls)    
     result = ballCount - dumbBallCount    
-    print("the computer takes away", dumbBallCount)    #Prints the random value to be subtracted from the pile    
+    print("The computer takes away", dumbBallCount,"balls.")    #Prints the random value to be subtracted from the pile    
     return result     
     result = computerDumbMode(ballCount)
 
-## Computes the ball count while in smart mode
-# 
+## Computes the ball count while in smart mode 
 # @return the ball count while in smart mode
-# Bryan and some of Jim
+# Bryan and Jim
 def computerSmartMode(ballCount) :
     if  10 <= ballCount <= 28:
         return 7
@@ -131,16 +132,16 @@ def computerSmartMode(ballCount) :
 def playerTurn (ballCount):
     playerTurn = False
     while not playerTurn:
-        print("The size of the ball pile is currently:", ballCount,"balls")
+        print("The size of the ball pile is currently:", ballCount,"balls.")
         allowableChoice = ballCount//2
-        print("How many balls would you like to remove?  Half of the pile size is: ", allowableChoice)
+        print("How many balls would you like to remove?  Half of the pile size is: ", allowableChoice,"balls.")
         playerChoice = input("Player chooses to remove: ")
         if playerChoice.isdigit() != True:   
            print("Your input was either negative or was not a number.  Try again :(" ) 
         else:
             playerChoice = int(playerChoice)
             if ballCount/2 < playerChoice or playerChoice == 0:
-                print("Dude.  Seriously.  Follow directions.")
+                print("Please follow directions.","\n")
             else:
                 ballCount -= playerChoice
                 playerTurn = True
@@ -155,7 +156,7 @@ tests if the player input a non digit
 input-
 Player chooses to remove: two
 Output-
-Dude.  Seriously.  Follow directions.
+Please follow directions.
 -program loops until the player follow directions
 
 Test Case2
